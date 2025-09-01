df05b64bfe33e6176c446f188c33a56ecf2bc246
Eduardo <eduardoorellana@hotmail.com>
2025-09-01 18:41:15 -0400

    Grabo todo y sellado para que no se arranque nada de las respuestas

--- src/aeropuertos/dto/create-aeropuerto.dto.ts
+++ src/aeropuertos/dto/create-aeropuerto.dto.ts
@@ -1,10 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
 
+// para crear nuevo aeropuertto , requiere nombre, codigo del pais y ciudad
 export class CreateAeropuertoDto {
-  @ApiProperty({ description: 'Nombre completo del aeropuerto', example: 'Aeropuerto Internacional Arturo Merino Benítez' })
+  @ApiProperty({ description: 'Nombre completo del aeropuerto', example: 'Aeropuerto Internacional Arturo Merino Benitez' })
   nombre: string;
 
-  @ApiProperty({ description: 'Código IATA de tres letras', example: 'SCL' })
+  @ApiProperty({ description: 'Codigo IATA de tres letras', example: 'SCL' })
   codigo: string;
 
   @ApiProperty({ description: 'Ciudad donde se encuentra el aeropuerto', example: 'Santiago' })
--- src/pasajeros/dto/create-pasajero.dto.ts
+++ src/pasajeros/dto/create-pasajero.dto.ts
@@ -1,1 +1,12 @@
-export class CreatePasajeroDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreatePasajeroDto {
+  @ApiProperty({ description: 'Nombre del pasajero', example: 'Alexis' })
+  nombre: string;
+
+  @ApiProperty({ description: 'Apellido del pasajero', example: 'Sanchez' })
+  apellido: string;
+
+    @ApiProperty({ description: 'Email del pasajero', example: 'alexis.sanchez@email.com' })
+    email: string;
+  }
